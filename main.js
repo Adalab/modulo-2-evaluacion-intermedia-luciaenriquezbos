e50b07bd1fce1075565372d2dd8f9576eaea52c8
luciaenriquezbos <eboscreative@gmail.com>
2021-05-12 10:33:36 +0200

    create button prueba

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 'use strict';
+
+const valueInput = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+const track = document.querySelector(".js-track");
+
+
 //Generar número aleatorio:::::::::::::
 
 const numberResult = getRandomNumber(100);
@@ -9,34 +15,42 @@ const numberResult = getRandomNumber(100);
 console.log("MI número aleatorio es "+ numberResult);
 
 //Contenido input
-const valueInput = document.querySelector(".js-input");
 
-const numberInput = valueInput.innerHTML ;
+function handleClickButton() {
 
-fuction handleClickButton() {
+    const numberInput = valueInput.value ;
 
+    console.log(numberInput);
 
+    //compara el numero
 
+    if (numberInput == numberResult){
+        track.innerHTML =`Has ganado Campeona!!!`;
+     }
+     else if (numberInput > 100){
+        track.innerHTML =`El número debe estar entre 1 y 100`;
+     }
+     else if (numberInput < 1){
+        track.innerHTML =`El número debe estar entre 1 y 100`;
+     }
+     else if (numberInput < numberResult){
+        track.innerHTML =`Demasiado bajo`;
+     }
+     else if (numberInput > numberResult){
+        track.innerHTML =`Demasiado alto`;
+     }
+     
+    
 }
-
-
-
 
 button.addEventListener( 'click' , handleClickButton );
 
 
 
 
-
-
-
-
-
-
-
 //Mostrar consola contenido input
 
-console.log(numberInput);
+//console.log(numberInput);
 
 
 
