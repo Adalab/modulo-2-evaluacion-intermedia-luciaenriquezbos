708a26b558a698d16a53cc8eb44abe075c3614f2
luciaenriquezbos <eboscreative@gmail.com>
2021-05-12 10:07:50 +0200

    add input value

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 'use strict';
+//Generar número aleatorio:::::::::::::
+
+const numberResult = getRandomNumber(100);
+
+
+//Mostrar número en consola::::::
+
+console.log("MI número aleatorio es "+ numberResult);
+
+//Contenido input
+const valueInput = document.querySelector(".js-input");
+
+const numberInput = valueInput.innerHTML ;
+
+fuction handleClickButton() {
+
+
+
+}
+
+
+
+
+button.addEventListener( 'click' , handleClickButton );
+
+
+
+
+
+
+
+
+
+
+
+//Mostrar consola contenido input
+
+console.log(numberInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }
+
+
